a45c67a3d773bd42b0ed0486dafc690fce8d55cc
Nana KOITA <koita.nana19@gmail.com>
2026-03-05 16:36:00 +0100

    maintenance

--- src/utils/cascade_delete.ts
+++ src/utils/cascade_delete.ts
@@ -7,25 +7,28 @@ type RelationAttribute = {
 }
 
 export async function cascadeDelete(uid: string, id: number, visited = new Set()) {
-    if (visited.has(`${uid}:${id}`)) return
-    visited.add(`${uid}:${id}`)
-
     const model = strapi.getModel(uid as any)
+    console.log(model)
+    // if (visited.has(`${uid}:${id}`)) return
+    // visited.add(`${uid}:${id}`)
 
-    for (const [_, attribute] of Object.entries(model.attributes)) {
-        const attr = attribute as RelationAttribute
+    // const model = strapi.getModel(uid as any)
 
-        if (!attr.mappedBy && !attr.inversedBy) continue
-        if (attr.type !== 'relation') continue
-        if (!attr.target) continue
+    // for (const [_, attribute] of Object.entries(model.attributes)) {
+    //     const attr = attribute as RelationAttribute
 
-        const targetUID = attr.target
-        const foreignKey = attr.mappedBy || attr.inversedBy
-        const relationEntries = await strapi.db.query(targetUID).findMany({ where: { [foreignKey]: id } })
+    //     if (!attr.mappedBy && !attr.inversedBy) continue
+    //     if (attr.type !== 'relation') continue
+    //     if (!attr.target) continue
 
-        for (const entry of relationEntries) {
-            await cascadeDelete(targetUID, entry.id, visited)
-            await strapi.db.query(targetUID).delete({ where: { id: entry.id } })
-        }
-    }
+    //     const targetUID = attr.target
+    //     const foreignKey = attr.mappedBy || attr.inversedBy
+    //     const relationEntries = await strapi.db.query(targetUID).findMany({ where: { [foreignKey]: id } })
+
+    //     for (const entry of relationEntries) {
+    //         await cascadeDelete(targetUID, entry.id, visited)
+    //         await strapi.db.query(targetUID).delete({ where: { id: entry.id } })
+    //     }
+    // }
+
 }
